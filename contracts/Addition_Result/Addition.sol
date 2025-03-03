4d2a0c36479b757ff6fa94703b0434f5fdd0445a
RicardoMuchacho <ricardojrmuchacho@gmail.com>
2025-03-03 18:55:56 +0100

    admin security added

--- contracts/Addition_Result/Addition.sol
+++ contracts/Addition_Result/Addition.sol
@@ -3,16 +3,25 @@
 pragma solidity 0.8.24;
 
 import "./IResult.sol";
+
 contract Additioner {
     address private resultContract;
-
+    address public admin;
+    uint256 public fee = 5;
     constructor(address _resultContract){
        resultContract = _resultContract;
+       admin = msg.sender;
     }
-
-    function add(uint256 n) public {
+    modifier onlyAdmin() {
+      if (msg.sender != admin) revert();
+      _;
+    }
+    function add(uint256 n) public  {
        IResult resultObj = IResult(resultContract);
        uint256 addition = resultObj.result() + n;
        resultObj.setResult(addition);
+    }
+    function changeFee(uint256 n) public onlyAdmin {
+       fee = n;
     }
 }
